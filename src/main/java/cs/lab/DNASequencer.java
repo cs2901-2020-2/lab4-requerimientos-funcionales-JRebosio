8a72a332c014588f0ffe32e36b3599076857442c
jorge rebosio <jorge.rebosio@utec.edu.pe>
2020-09-16 10:39:43 -0500

    add

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -9,8 +9,35 @@ public class  DNASequencer {
     public DNASequencer() {
         logger.info("Starting sequencer...");
     }
+   
 
+    public static String SCSLength(String X, String Y, int m, int n)
+    {
+        if (m == 0 || n == 0) {
+		return X.substring(0,m) + Y.substring(0,n);
+	}
+
+		
+	if (X.charAt(m - 1) == Y.charAt(n - 1)) {
+		return SCSLength(X, Y, m - 1, n - 1) + Y.charAt(n - 1);
+	}
+
+	String a = SCSLength(X, Y, m, n - 1) + Y.charAt(n - 1);
+	String b = SCSLength(X, Y, m - 1, n) + X.charAt(m - 1);
+		
+	if(a.length()> b.length()) return b;
+	else return a;
+    }
+		
     public String calculate(List<String> part){
-        return "AGATTACAGA";
+	
+	String X=part.get(0);
+	String Y=part.get(1);
+	String Z=part.get(2);
+		
+	String R1=SCSLength(X, Y, X.length(), Y.length());
+	String R2=SCSLength(R1, Z, R1.length(), Z.length());
+		
+	return R2;	
     }
 }
